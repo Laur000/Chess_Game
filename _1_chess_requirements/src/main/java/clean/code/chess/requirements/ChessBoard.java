d5bb0e00e9c0d7b4b4f936ee6ce16448d35031fe
Laur000 <laurlaurbagiu1.0@yahoo.com>
2020-03-10 22:58:51 +0200

    Chess_game

---
+++ _1_chess_requirements/src/main/java/clean/code/chess/requirements/ChessBoard.java
@@ -0,0 +1,37 @@
+package clean.code.chess.requirements;
+
+public class ChessBoard {
+
+    public static int MAX_BOARD_WIDTH = 7;
+    public static int MAX_BOARD_HEIGHT = 7;
+
+    private Pawn[][] pieces;
+
+    public ChessBoard() {
+        pieces = new Pawn[MAX_BOARD_WIDTH][MAX_BOARD_HEIGHT];
+
+    }
+
+    public void Add(Pawn pawn, int xCoordinate, int yCoordinate, PieceColor pieceColor) {
+        if(IsLegalBoardPosition(xCoordinate , yCoordinate) == true) {
+                pawn.setXCoordinate(xCoordinate);
+                pawn.setYCoordinate(yCoordinate);
+                this.pieces[xCoordinate][yCoordinate] = pawn ;
+        }
+        else
+        {
+            pawn.setXCoordinate(-1);
+            pawn.setYCoordinate(-1);
+        }
+    }
+
+    public boolean IsLegalBoardPosition(int xCoordinate, int yCoordinate) {
+        if(xCoordinate >= 0 && xCoordinate <7 && yCoordinate >= 0 && yCoordinate  < 7 ){
+            return true ;
+        }
+        else
+            {
+            return false;
+            }
+        }
+}
